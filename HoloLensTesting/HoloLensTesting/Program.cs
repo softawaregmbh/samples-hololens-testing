9266c0d9af05cb413b22073ab3be50e88bdbcf11
Roman Schacherl <roman.schacherl@softaware.at>
2016-09-19 13:19:24 +0200

    Playing recorded file and simulating gestures.

--- HoloLensTesting/HoloLensTesting/Program.cs
+++ HoloLensTesting/HoloLensTesting/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.PerceptionSimulation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,37 @@ namespace HoloLensTesting
     {
         static void Main(string[] args)
         {
+            AutomateHoloLens();
+
+            //PlayRecordedFile();
+
+            Console.WriteLine("Press any key to stop.");
+            Console.ReadKey();
+        }
+
+        private static async Task AutomateHoloLens()
+        {
+            var manager =PerceptionSimulationManager.CreatePerceptionSimulationManager(
+                new SimulationStreamSinkFactory().CreateSimulationStreamSink());
+
+            for (int i = 0; i < 5; i++)
+            {
+                manager.Human.RightHand.PerformGesture(SimulatedGesture.FingerPressed);
+                await Task.Delay(300);
+                manager.Human.RightHand.PerformGesture(SimulatedGesture.FingerReleased);
+
+                await Task.Delay(1000);
+                manager.Human.Head.Rotate(new Rotation3(0f, 0.2f, 0.0f));
+
+                await Task.Delay(300);
+            }
+        }
+
+        private static void PlayRecordedFile()
+        {
+            var recording = PerceptionSimulationManager.LoadPerceptionSimulationRecording(@"OpenBrowser.xef", new SimulationStreamSinkFactory());
+
+            recording.Play();
         }
     }
 }
---
+++ HoloLensTesting/HoloLensTesting/SimulationStreamSinkFactory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.PerceptionSimulation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoloLensTesting
+{
+    public class SimulationStreamSinkFactory : ISimulationStreamSinkFactory
+    {
+        public ISimulationStreamSink CreateSimulationStreamSink()
+        {
+            var task = Task.Run(async () => 
+                            await RestSimulationStreamSink.Create(
+                                    // use the IP address for your device/emulator
+                                    new Uri("http://169.254.106.72/"),
+                                    // no credentials are needed for the emulator
+                                    new System.Net.NetworkCredential("", ""),
+                                    // normal priorty
+                                    true,
+                                    // cancel token
+                                    new System.Threading.CancellationToken())
+                                    );
+
+            return task.Result;
+        }
+    }
+}
