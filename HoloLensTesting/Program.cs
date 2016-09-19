be52d8d930aa292b752e4c19c4c611a196c456b4
Roman Schacherl <roman.schacherl@softaware.at>
2016-09-19 14:03:58 +0200

    Removed code to set up initial template

--- HoloLensTesting/Program.cs
+++ HoloLensTesting/Program.cs
@@ -23,27 +23,12 @@ namespace HoloLensTesting
 
         private static async Task AutomateHoloLens()
         {
-            var manager =PerceptionSimulationManager.CreatePerceptionSimulationManager(
-                new SimulationStreamSinkFactory(deviceIpAddress).CreateSimulationStreamSink());
 
-            for (int i = 0; i < 5; i++)
-            {
-                manager.Human.RightHand.PerformGesture(SimulatedGesture.FingerPressed);
-                await Task.Delay(300);
-                manager.Human.RightHand.PerformGesture(SimulatedGesture.FingerReleased);
-
-                await Task.Delay(1000);
-                manager.Human.Head.Rotate(new Rotation3(0f, 0.2f, 0.0f));
-
-                await Task.Delay(300);
-            }
         }
 
         private static void PlayRecordedFile()
         {
-            var recording = PerceptionSimulationManager.LoadPerceptionSimulationRecording(@"OpenBrowser.xef", new SimulationStreamSinkFactory(deviceIpAddress));
 
-            recording.Play();
         }
     }
 }
